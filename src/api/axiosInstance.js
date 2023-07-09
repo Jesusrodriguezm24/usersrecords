7bff8aa50b7d197e4f1d8826d8d665bb370fa625
Jesus Rodriguez <jesusrodriguezm24@gmail.com>
2023-07-09 13:02:53 -0400

    cambiando URL en axiosInstance

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,3 +1,6 @@
 import axios from "axios";
 
-export const axiosInstance = axios.create({baseURL:"https://usersrecords.onrender.com/"});
+export const axiosInstance = axios.create({baseURL:"https://users-crud.academlo.tech"});
+
+//https://users-crud.academlo.tech/
+//https://usersrecords.onrender.com/
